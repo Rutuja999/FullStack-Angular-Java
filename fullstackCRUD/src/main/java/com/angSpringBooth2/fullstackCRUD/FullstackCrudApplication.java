5b1baf7c26fe834fd39f5ad607aafac34db100ea
Rutuja Satpute <resmmm99@gmail.com>
2024-07-26 12:05:17 +0530

    Complete changes for backend

--- fullstackCRUD/src/main/java/com/angSpringBooth2/fullstackCRUD/FullstackCrudApplication.java
+++ fullstackCRUD/src/main/java/com/angSpringBooth2/fullstackCRUD/FullstackCrudApplication.java
@@ -2,6 +2,7 @@ package com.angSpringBooth2.fullstackCRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class FullstackCrudApplication {
--- fullstackCRUD/src/main/java/com/sngSpringBoot/daoService/UserService.java
+++ fullstackCRUD/src/main/java/com/sngSpringBoot/daoService/UserService.java
@@ -1,6 +1,7 @@
 package com.sngSpringBoot.daoService;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,39 @@ import com.angSpringBoot.entity.User;
 public class UserService {
 	@Autowired
 	private UserRepo userRepo;
-	public List<User> createUser(List<User> user) {
-		return userRepo.saveAll(user);
+
+	public User createUser(User user) {
+		return userRepo.save(user);
 	}
+	
+	public List<User> createUsers(List<User> users) {
+		return userRepo.saveAll(users);
+	}
+
 	public User getUserById(int id) {
 		return userRepo.findById(id).orElse(null);
+	}
+
+	public List<User> getUsers() {
+		return userRepo.findAll();
+	}
+
+	public User updateUser(User user) {
+		User oldUser = null;
+		Optional<User> optionaluser = userRepo.findById(user.getId());
+		if (optionaluser.isPresent()) {
+			oldUser = optionaluser.get();
+			oldUser.setName(user.getName());
+			oldUser.setAddress(user.getAddress());
+			userRepo.save(oldUser);
+		} else {
+			return new User();
+		}
+		return user;
+	}
+
+	public String deleteUserById(int id) {
+		userRepo.deleteById(id);
+		return "User got deleted";
 	}
 }
